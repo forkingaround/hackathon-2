01a8b0cbde0a404827e58eb7a5297dbb2275afb4
Sanjiv Putty <sendurmsg2mehere@gmail.com>
2023-06-05 10:24:21 +0400

    update

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 import fs from 'node:fs';
 
-const projects = [];
-
+let project = new Array();
 fs.readFile('projects.txt', 'utf-8', function (err, data) {
     if (err) {
         console.error(err);
         return;
     }
-    projects = data.split(',');
+    try {
+        project = JSON.parse(data);
+        console.log(project[0]);
+    } catch (error) {
+        console.error('Invalid JSON string:', error);
+    }
 });
 
-console.log(projects);
+// console.log(projects);
+
+
+import express from 'express';
+// create a server 
+const app = express();
+const port = 3000;
+// set the route
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
